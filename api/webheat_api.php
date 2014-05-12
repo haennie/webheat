38622c12d5a3594230548bada41a97b1b915157b
haennie <hennie@kocham-cie.de>
2014-05-12 20:18:41 +0200

    oven on/off

--- api/webheat_api.php
+++ api/webheat_api.php
@@ -1,6 +1,5 @@
 <?php
-
-    require_once '../include/service/config.php';
+    require_once '../include/system/config.php';
 	require_once 'api_class.php';
 	
 	foreach ($_POST as $key => $value) {
