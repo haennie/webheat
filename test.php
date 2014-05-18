d5e23711338eb3f1baf3fa801e01b69dbdf6a529
haennie <hennie@kocham-cie.de>
2014-05-18 17:32:04 +0200

    oven on/off

--- test.php
+++ test.php
@@ -5,7 +5,7 @@
  * Date: 11.05.14
  * Time: 14:31
  */
-var_dump('aber drinn bin ich');
+var_dump('aber drinn bin ich');die();
 
 $oven_test = new sensor();
 $status = $oven_test->curl_request(array(
